14fad7c0d763a2ec12424a1b73bea8fbc9ee4804
Qosdil A <qosdil@kudo.co.id>
2018-12-01 17:46:31 +0700

    Fix "Host key verification failed.".

--- install-codegangsta-gin-cafe2ce.sh
+++ install-codegangsta-gin-cafe2ce.sh
@@ -3,7 +3,7 @@ dir=$GOPATH/src/github.com/codegangsta/gin
 rm -rf $dir
 mkdir -p $dir
 cd $dir
-git clone git@github.com:codegangsta/gin.git .
+git clone https://github.com/codegangsta/gin.git .
 git reset --hard cafe2ce
 go install -i .
 echo "Installing codegangsta/gin version \"cafe2ce\" was successful."
