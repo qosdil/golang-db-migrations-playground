a0e87a96f29f999259b87a7d4bc6a41c0094a93e
Qosdil A <qosdil@kudo.co.id>
2019-06-04 12:53:46 +0700

    Fix goose installer.

--- install-pressly-goose-2.3.0.sh
+++ install-pressly-goose-2.3.0.sh
@@ -1,4 +1,26 @@
-go get -v github.com/pressly/goose/cmd/goose
+# Prerequisites
+# - $GOPATH set up properly
+# - curl
+# - Git
+# - Connection to github.com
+# - Go (runs well on Go v1.11)
+#
+# Run: sh ./[this file]
+
+# Set up Go environment
+mkdir -p $GOPATH/bin
+mkdir -p $GOPATH/pkg
+mkdir -p $GOPATH/src
+
+# Install Dep
+curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
+
+# Install goose v2.3.0
+mkdir -p $GOPATH/src/github.com/pressly/goose
 cd $GOPATH/src/github.com/pressly/goose
+git clone git@github.com:pressly/goose.git .
 git checkout tags/v2.3.0
+dep ensure -v
+
+# Create goose binary
 go install -i ./cmd/goose
